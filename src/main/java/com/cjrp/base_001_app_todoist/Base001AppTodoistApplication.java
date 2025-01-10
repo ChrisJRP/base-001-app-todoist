bc9c31fcb3d343ae2c6d36e3b69eeab5c8c06d91
ChrisJRP <christianramirezpinco@gmail.com>
2025-01-10 13:07:03 -0500

    primer commit

---
+++ src/main/java/com/cjrp/base_001_app_todoist/Base001AppTodoistApplication.java
@@ -0,0 +1,13 @@
+package com.cjrp.base_001_app_todoist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Base001AppTodoistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Base001AppTodoistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjrp/base_001_app_todoist/Base001AppTodoistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjrp.base_001_app_todoist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base001AppTodoistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
